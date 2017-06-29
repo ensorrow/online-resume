638966e8a617070907d182e72a46297e2487bf4d
ensorrow <1121264060@qq.com>
2017-06-29 18:34:17 +0800

    first commit

---
+++ js/data.js
@@ -0,0 +1,94 @@
+render({
+  header: {
+    name: '吕哲扬',
+    slogan: '求职目标： 前端开发工程师',
+    age: 20,
+    avatar: 'imgs/avatar-c.png',
+    address: '江西省上饶市',
+    mobile: '18292876059',
+    email: '18292876059@163.com',
+    website: 'http://lvzheyang.top',
+    github: 'https://github.com/ensorrow/'
+  },
+  aside: [
+    {
+      title: '职业技能',
+      content: [
+        '熟练掌握HTML, CSS, JavaScript',
+        '熟练运用React, Vue, 微信小程序等MVVM框架',
+        '熟悉Node.js后端开发',
+        '了解Python, PHP, Java等其他语言'
+      ]
+    },
+    {
+      title: '技能证书',
+      content: [
+        'CET-6(570分)',
+        'C语言二级证书',
+        'MS Office二级证书(优秀)',
+        'Photoshop一级证书'
+      ]
+    },
+    {
+      title: '个人评价',
+      content: ['热爱前端，在校期间实践大量项 目，React、Vue、Angular等多 种主流框架均有实际项目经验。 喜欢通过写博客来进行深入的学 习和总结。在校期间接手简单的 后端任务，课余时间会写一些爬 虫之类的小玩具。在网易实习期 间对工作有极大热情，与团队相 处非常融洽。']
+    }
+  ],
+  main: [
+    {
+      title: '教育经历',
+      content: [
+        {
+          timeRange: '2014.09至今',
+          place: '西安交通大学',
+          remark: '过程装备与控制工程'
+        }
+      ]
+    },
+    {
+      title: '实习经验',
+      content: [
+        {
+          timeRange: '2016.07-2016.09',
+          place: '网易杭州研究院',
+          remark: '前端开发实习生',
+          introduction: '在大二时通过筛选成为网易2016年春招实习生，暑假期间在网易杭州 研究院-前端技术部-前端支持组实习，实习期间主要完成了网易蜂巢 旧ICP备案系统的维护及新ICP备案系统(icp.c.163.com)的上线'
+        }
+      ]
+    },
+    {
+      title: '校内实践',
+      content: [
+        {
+          timeRange: '2016.05-2017.05',
+          place: 'e瞳网',
+          remark: '前端主管',
+          introduction: '负责校e瞳网前端部分的更新维护，承接校内外项目赚取社团经费，招纳培养新人实现技术传承，致力于提升交大整体的前端水平。'
+        },
+        {
+          timeRange: '2016.05-2017.05',
+          place: '西安映射网络科技公司',
+          remark: '技术总监',
+          introduction: '加入创业团队，负责公司手机端产品开发，包括手机网页，iOS开发(使用React Native);负责团队开发人员的培训与管理调度。'
+        }
+      ]
+    },
+    {
+      title: '项目经验',
+      content: [
+        {
+          timeRange: '2016.10-2016.11',
+          place: '网安实验室云安全平台',
+          remark: '',
+          introduction: '为西安交通大学网安实验室外包项目制作前端页面作为数据展示，主 要展示形式为表格加图表，项目采用前端渲染，应用js模块化，引入 ejs模板引擎，使用sass预处理器，应用了较多的ES6语法'
+        },
+        {
+          timeRange: '2016.08-2016.09',
+          place: '网易蜂巢ICP备案系统',
+          remark: '',
+          introduction: '实习初期负责对旧ICP备案系统(使用React搭建)进行功能维护 (修复了大部分旧系统的BUG)和样式更新(引入了antd);后在导 师指导下，使用网易Regular框架全新搭建了新的备案系统，基本实 现了数据驱动与组件化的要求(导师提供了整体架构支持)'
+        }
+      ]
+    }
+  ]
+});
