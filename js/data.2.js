f275909a8c4e20e032c546270f7a1913dd76bd4a
ensorrow <1121264060@qq.com>
2017-07-31 10:48:30 +0800

    最终版简历

---
+++ js/data.2.js
@@ -0,0 +1,100 @@
+render({
+  header: {
+    name: '吕哲扬',
+    slogan: '',
+    age: 20,
+    avatar: 'imgs/avatar-c.png',
+    address: '江西省上饶市',
+    mobile: '18292876059',
+    email: '18292876059@163.com',
+    website: 'http://lvzheyang.top',
+    github: 'https://github.com/ensorrow/'
+  },
+  aside: [
+    {
+      title: '职业技能',
+      content: [
+        '熟练掌握HTML, CSS, JavaScript',
+        '熟练运用React, Vue, 微信小程序等MVVM框架',
+        '熟悉Node.js后端开发',
+        '了解Python, PHP, Java等其他编程语言'
+      ]
+    },
+    {
+      title: '技能证书',
+      content: [
+        'CET-6(570分)',
+        'C语言二级证书',
+        'MS Office二级证书(优秀)',
+        'Photoshop一级证书'
+      ]
+    },
+    {
+      title: '个人评价',
+      content: ['  热爱编程，在校期间主动参与开发了大量项目，React、Vue、Angular等多种主流前端MVVM框架均有实际项目经验。喜欢通过写博客来进行深入的学习和总结。参与过腾讯蓝鲸高校培训，有一定的Django后端开发经验；曾使用Node.js独立开发化工学院实验台预约系统后端。','  较好的沟通能力和一定的管理经验，担任社团主管与创业团队总监期间充分锻炼了自己的软实力，一定程度上能够主导项目开发、组织大小活动。在网易实习期间，与组内师兄师姐相处融洽，并受到过项目组内其他成员好评。','  学习能力较强，本科阶段一直能够坚持双线发展，在努力成为一名合格开发者的同时学好本专业，大一大二学年综排专业前20%。同时喜欢读书，空闲时间阅读过大量文学作品。英语水平较好，能够较流畅的阅读英文文档，看一般的英文视频。']
+    }
+  ],
+  main: [
+    {
+      title: '教育经历',
+      content: [
+        {
+          timeRange: '2014.09至今',
+          place: '西安交通大学',
+          remark: '过程装备与控制工程'
+        }
+      ]
+    },
+    {
+      title: '实习经验',
+      content: [
+        {
+          timeRange: '2016.07-2016.09',
+          place: '网易杭州研究院',
+          remark: '前端开发实习生',
+          introduction: '在大二时通过筛选成为网易2016年春招实习生，暑假期间在网易杭州研究院-前端技术部-前端支持组实习，实习期间主要完成了网易蜂巢旧ICP备案系统的维护及新ICP备案系统(icp.c.163.com)的上线'
+        }
+      ]
+    },
+    {
+      title: '校内实践',
+      content: [
+        {
+          timeRange: '2016.05-2017.05',
+          place: 'e瞳网',
+          remark: '前端主管',
+          introduction: '负责e瞳网各项目前端部分的更新维护，招纳培养新人实现技术传承；负责部门日常例会、培训，致力于提升交大整体的前端水平。'
+        },
+        {
+          timeRange: '2015.03-2016.05',
+          place: '西安映射网络科技公司',
+          remark: '技术总监',
+          introduction: '加入创业团队，负责公司手机端产品开发，包括手机网页，iOS开发(使用React Native);负责团队开发人员的招募、培训与管理调度。'
+        }
+      ]
+    },
+    {
+      title: '项目经验',
+      content: [
+        {
+          timeRange: '2016.10-2016.11',
+          place: '网安实验室云安全平台',
+          remark: '',
+          introduction: '为西安交通大学网安实验室外包项目制作前端页面作为数据展示，主要展示形式为表格加图表，项目采用gulp管理，应用js模块化（webpack实现），引入ejs模板引擎，使用sass预处理器，应用了较多的ES6语法'
+        },
+        {
+          timeRange: '2016.08-2016.09',
+          place: '网易蜂巢ICP备案系统',
+          remark: '',
+          introduction: '实习初期负责对旧ICP备案系统(使用React搭建)进行功能维护 (修复了大部分旧系统的BUG)和样式更新(引入了ant design框架);后在导师指导下，使用网易MVVM框架Regular全新搭建了新的备案系统，基本实现了数据驱动与组件化的要求(导师提供了整体架构支持)'
+        },
+        {
+          timeRange: '2015.10-2016.03',
+          place: '师友Web App & iOS App',
+          remark: '',
+          introduction: '创业团队产品，包括教师端、学生端；Web App使用jQuery Mobile搭建，大部分页面脱离设计独立完成，实现了移动端各屏幕尺寸的适配;iOS APP使用React Native开发，初步尝试组件化与函数式编程，采用Redux架构'
+        }
+      ]
+    }
+  ]
+});
